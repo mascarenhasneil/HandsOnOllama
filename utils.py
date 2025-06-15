98b76d9d7b6e6700aedbc627b5f57582056afa68
Neil Mascarenhas <neilpm1995@gmail.com>
2025-06-14 22:35:12 -0400

    feat: add utils.py for PDF ingestion and document splitting functions

---
+++ utils.py
@@ -0,0 +1,32 @@
+"""
+module: utils.py
+Utility functions for the Document Assistant application.
+This module provides functions to ingest PDF documents and split them into smaller chunks for processing.
+
+"""
+
+import os
+import logging
+from typing import List
+from langchain_community.document_loaders import UnstructuredPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+import streamlit as st
+
+def ingest_pdf(doc_path: str) -> List:
+    """Load PDF documents."""
+    if os.path.exists(doc_path):
+        loader = UnstructuredPDFLoader(file_path=doc_path)
+        data = loader.load()
+        logging.info("PDF loaded successfully.")
+        return data
+    else:
+        logging.error("PDF file not found at path: %s", doc_path)
+        st.error("PDF file not found at path")
+        return []
+
+def split_documents(documents: List) -> List:
+    """Split documents into smaller chunks."""
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1200, chunk_overlap=300)
+    chunks = text_splitter.split_documents(documents)
+    logging.info("Documents split into chunks.")
+    return chunks
